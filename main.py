2ea183001aca194f85c24e99518f649658b5f1cb
callumfrance <france.callum@gmail.com>
2018-10-20 15:20:16 +0800

    inputting patterns into dicts now as intended

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import re
+from string import ascii_lowercase
 
+"""
+First, finds the maximum size of a 'subword' for each letter of the alphabet
+"""
 al = [0] * 26
 with open('patterns.txt') as fo:
     for index, line in enumerate(fo):
@@ -12,30 +16,72 @@ with open('patterns.txt') as fo:
                 break
 print(al)
 
+al_l = [None] * 26
+for i, letter in enumerate(al):
+    al_l[i] = [None] * al[i]
+    for i2, x in enumerate(al_l[i]):
+        al_l[i][i2] = list()
+print(al_l)
+
+"""
+Creates a dictionary of lists of lists for each letter of the alphabet, where
+each nested nested list represents the length of a subword, and each nested
+list represents words that begin/end with the corresponding letter.
+"""
 prefixes = {
-    '.a' : [], '.b' : [], '.c' : [], '.d' : [], '.e' : [], '.f' : [], '.g' : [],
-    '.h' : [], '.i' : [], '.j' : [], '.k' : [], '.l' : [], '.m' : [], '.n' : [],
-    '.o' : [], '.p' : [], '.q' : [], '.r' : [], '.s' : [], '.t' : [], '.u' : [],
-    '.v' : [], '.w' : [], '.x' : [], '.y' : [], '.z' : [],
+        '.a' : al_l[0], '.b' : al_l[1], '.c' : al_l[2],
+        '.d' : al_l[3], '.e' : al_l[4], '.f' : al_l[5],
+        '.g' : al_l[6], '.h' : al_l[7], '.i' : al_l[8],
+        '.j' : al_l[9], '.k' : al_l[10], '.l' : al_l[11],
+        '.m' : al_l[12], '.n' : al_l[13], '.o' : al_l[14],
+        '.p' : al_l[15], '.q' : al_l[16], '.r' : al_l[17],
+        '.s' : al_l[18], '.t' : al_l[19], '.u' : al_l[20],
+        '.v' : al_l[21], '.w' : al_l[22], '.x' : al_l[23],
+        '.y' : al_l[24], '.z' : al_l[25],
 }
-word_bites = {
-    'a' : [], 'b' : [], 'c' : [], 'd' : [], 'e' : [], 'f' : [], 'g' : [],
-    'h' : [], 'i' : [], 'j' : [], 'k' : [], 'l' : [], 'm' : [], 'n' : [],
-    'o' : [], 'p' : [], 'q' : [], 'r' : [], 's' : [], 't' : [], 'u' : [],
-    'v' : [], 'w' : [], 'x' : [], 'y' : [], 'z' : [],
+subwords = {
+        'a' : al_l[0], 'b' : al_l[1], 'c' : al_l[2],
+        'd' : al_l[3], 'e' : al_l[4], 'f' : al_l[5],
+        'g' : al_l[6], 'h' : al_l[7], 'i' : al_l[8],
+        'j' : al_l[9], 'k' : al_l[10], 'l' : al_l[11],
+        'm' : al_l[12], 'n' : al_l[13], 'o' : al_l[14],
+        'p' : al_l[15], 'q' : al_l[16], 'r' : al_l[17],
+        's' : al_l[18], 't' : al_l[19], 'u' : al_l[20],
+        'v' : al_l[21], 'w' : al_l[22], 'x' : al_l[23],
+        'y' : al_l[24], 'z' : al_l[25],
 }
 suffixes = {
-    'a.' : [], 'b.' : [], 'c.' : [], 'd.' : [], 'e.' : [], 'f.' : [], 'g.' : [],
-    'h.' : [], 'i.' : [], 'j.' : [], 'k.' : [], 'l.' : [], 'm.' : [], 'n.' : [],
-    'o.' : [], 'p.' : [], 'q.' : [], 'r.' : [], 's.' : [], 't.' : [], 'u.' : [],
-    'v.' : [], 'w.' : [], 'x.' : [], 'y.' : [], 'z.' : [],
+        'a.' : al_l[0], 'b.' : al_l[1], 'c.' : al_l[2],
+        'd.' : al_l[3], 'e.' : al_l[4], 'f.' : al_l[5],
+        'g.' : al_l[6], 'h.' : al_l[7], 'i.' : al_l[8],
+        'j.' : al_l[9], 'k.' : al_l[10], 'l.' : al_l[11],
+        'm.' : al_l[12], 'n.' : al_l[13], 'o.' : al_l[14],
+        'p.' : al_l[15], 'q.' : al_l[16], 'r.' : al_l[17],
+        's.' : al_l[18], 't.' : al_l[19], 'u.' : al_l[20],
+        'v.' : al_l[21], 'w.' : al_l[22], 'x.' : al_l[23],
+        'y.' : al_l[24], 'z.' : al_l[25],
 }
+"""
+Goes through the patterns file and places the subwords into their appropriate
+slot in prefixes, suffixes, or subwords
+"""
 with open('patterns.txt') as fo:
     for line in fo:
         line = line.rstrip()
         if line.endswith('.'):
-            re.sub(r"[^a-zA-Z]+", '', line)
+            size = len(re.sub(r"[^a-zA-Z]+", '', line))
+            suffixes[line[-2:]][size-1].append(line)
         elif line.startswith('.'):
-            pass
+            size = len(re.sub(r"[^a-zA-Z]+", '', line))
+            prefixes[line[:2]][size-1].append(line)
         else:
-            pass
+            true_subword = re.sub(r"[^a-zA-Z]+", '', line)
+            size = len(true_subword)
+            subwords[true_subword[:1]][size-1].append(line)
+    print("\n-------------------------------------------------------------\n")
+    print(prefixes)
+    print("\n-------------------------------------------------------------\n")
+    print(subwords)
+    print("\n-------------------------------------------------------------\n")
+    print(suffixes)
+    print("\n-------------------------------------------------------------\n")
